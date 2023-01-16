51187fae56b2856a2fb3cfe2de6f0e98df33b93d
Martin Chan <martinchan53@gmail.com>
2023-01-16 14:25:23 +0000

    init: initiate class library

---
+++ Algebra.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cstoolbox
+{
+    public class Algebra
+    {
+        public double Addition(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Substraction(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Multiplication(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Division(double x, double y)
+        {
+            return x / y;
+        }
+    }
+
+}
---
+++ StringManipulation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace cstoolbox
+{
+    public class StringManipulation
+    {
+        public void Concatenate(string text1, string text2)
+        {
+            Console.WriteLine(text1, text2);
+        }
+
+        // See https://learn.microsoft.com/en-us/dotnet/core/tutorials/library-with-visual-studio?pivots=dotnet-6-0
+        public bool StartsWithUpper(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return false;
+
+            char ch = text[0];
+            return char.IsUpper(ch);
+        }
+
+        public string ReplaceSpaceWithUnderscore(string text)
+        {
+            string newtext;
+            newtext = text.Replace(" ", "_");
+            return newtext;
+        }
+
+    }    
+}
